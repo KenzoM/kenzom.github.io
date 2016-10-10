cb2d283284e7dee4f537fd56e2727d3013256d7d
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-10 15:01:19 -0700

    appended nav component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import style from '../stylesheets/styles.scss';
 
+import Header from './header';
+
 export default class App extends Component {
   render() {
     return (
-      <div>Test</div>
+      <Header />
     );
   }
 }
