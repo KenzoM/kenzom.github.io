f11d11d0436e282a491542243a8e92d05c4f0e78
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-11 13:40:55 -0700

    add center block class for img

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ export default class Header extends Component{
         <div className="container">
           <div className ="row">
             <div className="col-sm-12 col-md-4">
-              <img src="../src/image/kenzo.png" className="img-responsive img-circle" alt="Profile Picture" />
+              <img src="../src/image/kenzo.png" className="img-responsive img-circle center-block" alt="Profile Picture" />
             </div>
             <div className="col-sm-12  col-md-8  text-center">
               <h1>Hello there!</h1>
