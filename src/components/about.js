9931d7b94970cb7a72e52aa0b82c107a90a70e9a
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-10 20:35:17 -0700

    get rid of materalize

--- src/components/about.js
+++ src/components/about.js
@@ -3,18 +3,6 @@ import React, { Component } from 'react';
 export default class Header extends Component{
   render(){
     return (
-      <div className = "about border">
-        <div className ="container">
-          <div className="row">
-            <div className="col s3 border">
-              <img src="../src/image/kenzo.png" alt="profile" className="circle responsive-img" />
-            </div>
-            <div className="col s9 border">
-              <p> Hello, this is Kenzo</p>
-             </div>
-          </div>
-        </div>
-      </div>
     )
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,6 @@ import React, { Component } from 'react';
 export default class Header extends Component{
   render(){
     return (
-      <div>
-        <nav>
-          <div className="nav-wrapper container">
-            <a href="#" className="brand-logo">Kenzo</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Portfolio</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-
     )
   }
 }
