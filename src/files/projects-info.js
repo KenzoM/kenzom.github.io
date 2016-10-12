b44dac922d3d543fa77bcdd3821f50996df69c1c
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-12 11:34:46 -0700

    connected redux and react

---
+++ src/files/projects-info.js
@@ -0,0 +1,14 @@
+let arr = [];
+const Widget = function(picture,linkDemo,linkCode,projectTitle,projectDescription){
+  this.picture = picture;
+  this.linkDemo = linkDemo;
+  this.linkCode = linkCode;
+  this.projectTitle = projectTitle;
+  this.projectDescription = projectDescription;
+}
+let ex = new Widget('picture url','linkDemo here','flfd','w/e title','whatever description');
+arr.push(ex)
+
+let kenzo = {name:"kenzo"}
+
+module.export = kenzo;
