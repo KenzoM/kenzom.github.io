51fe1803aeed6ddd2986ef2045adb38dc818e4d2
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-11 17:55:21 -0700

    fixed some styles

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,19 +17,19 @@ export default class Projects extends Component {
               <p className="text-center"> Links </p>
             </div>
             <div className="project">
-              <img src="/src/image/simon.png" />
+              <img src="/src/image/heatmap.png" />
               <h3 className="project-title text-center">Title</h3>
               <h4 className="text-center"> This is some description </h4>
               <p className="text-center"> Links </p>
             </div>
             <div className="project">
-              <img src="/src/image/simon.png" />
+              <img src="/src/image/game-of-life.png" />
               <h3 className="project-title text-center">Title</h3>
               <h4 className="text-center"> This is some description </h4>
               <p className="text-center"> Links </p>
             </div>
             <div className="project">
-              <img src="/src/image/simon.png" />
+              <img src="/src/image/map.png" />
               <h3 className="project-title text-center">Title</h3>
               <h4 className="text-center"> This is some description </h4>
               <p className="text-center"> Links </p>
