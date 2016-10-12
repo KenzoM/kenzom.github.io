f9e740143bbd1b76c014f0c0523e494461f9adcf
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-12 14:22:01 -0700

    updated projects section

--- src/files/projects-info.js
+++ src/files/projects-info.js
@@ -1,16 +1,44 @@
 export const INFO = [
   {
+    picture: '/src/image/game-of-life.png' ,
+    linkProject: 'https://github.com/Neotriz/FCC-Game-of-Life' ,
+    linkDemo: 'https://game-of-life-fcc-project.herokuapp.com/' ,
+    projectTitle: 'Conway\' Game of Life' ,
+    projectDescription: '#React, #Redux, #ES6' ,
+  },
+  {
     picture: '/src/image/simon.png' ,
-    linkProject: 'https://google.com' ,
-    linkDemo: 'https://s.codepen.io/neotriz/debug/RRxOJb' ,
+    linkProject: 'https://github.com/Neotriz/SimonFCC' ,
+    linkDemo: 'http://s.codepen.io/neotriz/debug/RRxOJb' ,
     projectTitle: 'Simon Game' ,
-    projectDescription: 'Involves OO and jQuery' ,
+    projectDescription: '#Object-Oriented, #jQuery' ,
+  },
+  {
+    picture: '/src/image/tic-tac-toe.png' ,
+    linkProject: '#' ,
+    linkDemo: 'http://s.codepen.io/neotriz/debug/yJPLOO' ,
+    projectTitle: 'Tic-Tac-Toe' ,
+    projectDescription: '#Object-Oriented, #jQuery' ,
   },
   {
     picture: '/src/image/heatmap.png' ,
-    linkProject: 'https://bing.com' ,
+    linkProject: 'https://github.com/Neotriz/FCC-HeatMap' ,
     linkDemo: 'http://s.codepen.io/neotriz/debug/PGOLBb' ,
     projectTitle: 'HeatMap' ,
-    projectDescription: 'involes D3.js stuff' ,
+    projectDescription: '#d3.js' ,
   },
+  {
+    picture: '/src/image/weather.png' ,
+    linkProject: 'http://codepen.io/neotriz/pen/EywOQQ' ,
+    linkDemo: 'http://s.codepen.io/neotriz/debug/EywOQQ' ,
+    projectTitle: 'Local Weather' ,
+    projectDescription: '#ajax, #jQuery' ,
+  },
+  {
+    picture: '/src/image/recipe-box.png' ,
+    linkProject: 'https://bing.com' ,
+    linkDemo: 'https://google.com' ,
+    projectTitle: 'Recipe Box' ,
+    projectDescription: '#React, #Redux' ,
+  }
 ];
