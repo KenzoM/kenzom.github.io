1f46e344ddb8eeb75374da624b04b00a95779be3
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-12 11:57:22 -0700

    added reduce function

--- src/files/projects-info.js
+++ src/files/projects-info.js
@@ -1,14 +1,16 @@
-let arr = [];
-const Widget = function(picture,linkDemo,linkCode,projectTitle,projectDescription){
-  this.picture = picture;
-  this.linkDemo = linkDemo;
-  this.linkCode = linkCode;
-  this.projectTitle = projectTitle;
-  this.projectDescription = projectDescription;
-}
-let ex = new Widget('picture url','linkDemo here','flfd','w/e title','whatever description');
-arr.push(ex)
-
-let kenzo = {name:"kenzo"}
-
-module.export = kenzo;
+export const INFO = [
+  {
+    picture: '/src/image/simon.png' ,
+    linkProject: 'https://google.com' ,
+    linkDemo: 'https://s.codepen.io/neotriz/debug/RRxOJb' ,
+    projectTitle: 'Simon Game' ,
+    projectDescription: 'Involves OO and jQuery' ,
+  },
+  {
+    picture: '/src/image/heatmap.png' ,
+    linkProject: 'https://bing.com' ,
+    linkDemo: 'http://s.codepen.io/neotriz/debug/PGOLBb' ,
+    projectTitle: 'HeatMap' ,
+    projectDescription: 'involes D3.js stuff' ,
+  },
+];
