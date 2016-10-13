c3512b318fa424b780e25eabb7b2197d0305051c
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-12 22:16:33 -0700

    experimenting css animation

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,18 +3,14 @@ import React, { Component } from 'react';
 export default class Intro extends Component{
   render(){
     return (
-      <div className="jumbotron">
-        <div className="container">
-          <div className ="row">
-            <div className="col-sm-12  col-md-12  text-center">
-              <h1>Hello there!</h1>
-              <p>My name is Kenzo. I am a front-end web developer</p>
-              <p>Self-taught, but ambtious!</p>
-              <a href="https://www.dropbox.com/s/241w28bx3wvd7f4/Resume%202012%20Kenzo.pdf?dl=1"><i className="icon fa fa-file-pdf-o fa-5x"></i></a>
-              <a href="https://www.linkedin.com/in/kenzo-mendoza-654b1428"><i className="icon fa fa-linkedin-square fa-5x"></i></a>
-              <a href="https://codepen.io/neotriz/"><i className="icon fa fa-codepen fa-5x"></i></a>
-              <a href="https://github.com/neotriz"><i className="icon fa fa-github fa-5x"></i></a>
-            </div>
+      <div id="intro">
+        <div className="typewriter">
+          <h1>Hello world </h1>
+          <div className="home-icons">
+            <a href="https://www.dropbox.com/s/241w28bx3wvd7f4/Resume%202012%20Kenzo.pdf?dl=1"><i className="icon fa fa-file-pdf-o fa-5x"></i></a>
+            <a href="https://github.com/neotriz"><i className="icon fa fa-github fa-5x"></i></a>
+            <a href="https://codepen.io/neotriz/"><i className="icon fa fa-codepen fa-5x"></i></a>
+            <a href="https://www.linkedin.com/in/kenzo-mendoza-654b1428"><i className="icon fa fa-linkedin-square fa-5x"></i></a>
           </div>
         </div>
       </div>
