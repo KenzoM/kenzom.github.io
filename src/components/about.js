0a0e01c3aed924ec748296ad067dd83121a6c3fd
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-13 16:04:48 -0700

    decided to remove carousel

--- src/components/about.js
+++ src/components/about.js
@@ -5,51 +5,6 @@ export default class About extends Component {
     return(
       <div>
         <span className="anchor" id="about-section"></span>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <h1>About</h1>
-              <hr />
-            </div>
-            <div className="col-sm-12 text-center">
-              <div id="myCarousel" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                  <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                  <li data-target="#myCarousel" data-slide-to="1"></li>
-                  <li data-target="#myCarousel" data-slide-to="2"></li>
-                  <li data-target="#myCarousel" data-slide-to="3"></li>
-                </ol>
-                <div className="carousel-inner" role="listbox">
-                  <div className="item active">
-                    <img className ="img-responsive center-block" src="https://unsplash.it/200/300/?random" alt="Chania" />
-                  </div>
-
-                  <div className="item">
-                    <img className ="img-responsive center-block" src="https://unsplash.it/200/300/?random" alt="Chania" />
-                  </div>
-
-                  <div className="item">
-                    <img className ="img-responsive center-block" src="https://unsplash.it/200/300/?random" alt="Flower" />
-                  </div>
-
-                  <div className="item">
-                    <img className ="img-responsive center-block" src="https://unsplash.it/200/300/?random" alt="Flower" />
-                  </div>
-                </div>
-
-                <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                  <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                  <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
       </div>
     )
   }
