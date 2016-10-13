59059a14dfbe0f5033596277b0c30748d1a6cd43
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-13 16:52:21 -0700

    added and filled about section

--- src/components/about.js
+++ src/components/about.js
@@ -5,6 +5,29 @@ export default class About extends Component {
     return(
       <div>
         <span className="anchor" id="about-section"></span>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h1>About</h1>
+              <hr />
+            </div>
+            <div className="col-sm-12 col-md-6 text-center">
+              <img className="img-responsive center-block bio-picture" src="src/image/kenzo.png" />
+            </div>
+            <div className="col-sm-12 col-md-6 text-center">
+              <h3>Hi! My name is <span className="highlight-text"> Kenzo</span></h3>
+              <p className="about-bio">I&#39;ve learned how to program
+                through various, most notably FreeCodeCamp, Udemy and Udacity.
+                <br />
+                I specialize in front-end but I am currently working on more back-end
+                to become a full-stack web-developer.
+                <br />
+                In my spare time, I go out running here in San Francisco, travel around the world,
+                and play flamenco music.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
