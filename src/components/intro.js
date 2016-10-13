cb12d620d01b86b1bd40b1da316a8e1bb7f6c777
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-12 23:01:44 -0700

    tinkering with animation

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,12 +5,14 @@ export default class Intro extends Component{
     return (
       <div id="intro">
         <div className="typewriter">
-          <h1>Hello world </h1>
+          <img src="src/image/kenzo.svg" />
+          <h1>Hello world!</h1>
           <div className="home-icons">
             <a href="https://www.dropbox.com/s/241w28bx3wvd7f4/Resume%202012%20Kenzo.pdf?dl=1"><i className="icon fa fa-file-pdf-o fa-5x"></i></a>
             <a href="https://github.com/neotriz"><i className="icon fa fa-github fa-5x"></i></a>
             <a href="https://codepen.io/neotriz/"><i className="icon fa fa-codepen fa-5x"></i></a>
             <a href="https://www.linkedin.com/in/kenzo-mendoza-654b1428"><i className="icon fa fa-linkedin-square fa-5x"></i></a>
+            <a href="https://twitter.com/KenzoJam"><i className="icon fa fa-twitter fa-5x"></i></a>
           </div>
         </div>
       </div>
