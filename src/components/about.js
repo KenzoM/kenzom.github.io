db893e8154a1c1b6a33b51940ed47fdf85468039
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-13 15:06:21 -0700

    added carousel function

--- src/components/about.js
+++ src/components/about.js
@@ -5,11 +5,52 @@ export default class About extends Component {
     return(
       <div>
         <span className="anchor" id="about-section"></span>
-        <div className="jumbotron">
-          <h1>Bootstrap Tutorial</h1>
-          <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive,
-          mobile-first projects on the web.</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h1>About</h1>
+              <hr />
+            </div>
+            <div className="col-sm-12 text-center">
+              <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <div className="carousel-inner" role="listbox">
+                  <div className="item active">
+                    <img src="src/image/kenzo.png" alt="Chania" />
+                  </div>
+
+                  <div className="item">
+                    <img src="src/image/kenzo.png" alt="Chania" />
+                  </div>
+
+                  <div className="item">
+                    <img src="src/image/kenzo.png" alt="Flower" />
+                  </div>
+
+                  <div className="item">
+                    <img src="src/image/kenzo.png" alt="Flower" />
+                  </div>
+                </div>
+
+                <a className="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                  <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a className="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                  <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
     )
   }
