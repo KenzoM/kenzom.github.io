7e70d4564c24c7e01e607ecc34469c9581ab40db
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-14 20:50:16 -0700

    fixed typo for auto scroll

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ export default class Header extends Component{
       <div className="navbar navbar-default navbar-fixed-top" role="navigation">
         <div className="container">
             <div className="navbar-header">
-              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span className="sr-only">Toggle navigation</span>
                 <span className="icon-bar"></span>
                 <span className="icon-bar"></span>
@@ -16,12 +16,20 @@ export default class Header extends Component{
               </button>
               <Link activeClass="active" className="Intro navbar-brand nav-cursor" to="intro-section" spy={true} smooth={true} offset={-45} duration={1000} >Kenzo Logo</Link>
             </div>
-            <div className="navbar-collapse collapse">
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav-cursor nav navbar-nav navbar-right">
-                <li><Link activeClass="active" className="About" to="about-section" spy={true} smooth={true} offset={-45} duration={1000} >About</Link></li>
-                <li><Link activeClass="active" className="Skills" to="skills-section" spy={true} smooth={true} offset={-45} duration={1000} >Skills</Link></li>
-                <li><Link activeClass="active" className="Project" to="project-section" spy={true} smooth={true} offset={-45} duration={1000} >Project</Link></li>
-                <li><Link activeClass="active" className="Contact" to="contact-section" spy={true} smooth={true} offset={-45} duration={1000} >Contact</Link></li>
+                <li>
+                  <Link activeClass="active" className="About" to="about-section" spy={true} smooth={true} offset={-45} duration={1000} >About</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" className="Skills" to="skills-section" spy={true} smooth={true} offset={-45} duration={1000} >Skills</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" className="Project" to="project-section" spy={true} smooth={true} offset={-45} duration={1000} >Project</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" className="Contact" to="contact-section" spy={true} smooth={true} offset={-45} duration={1000} >Contact</Link>
+                </li>
               </ul>
             </div>
         </div>
