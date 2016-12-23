a601fec107b5b3a89e771366fbfcc05e1ba92fef
Kenzo Mendoza <kenzom954@gmail.com>
2016-12-23 17:32:51 -0500

    removed links in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,11 +8,6 @@ export default class Footer extends Component{
           <div className="navbar-text pull-left footer-copyright">
             © 2016 Kenzo Mendoza
           </div>
-          <div className="navbar-text pull-right">
-      			<a href="https://www.linkedin.com/in/kenzo-mendoza-654b1428"><i className="icon fa fa-linkedin fa-2x"></i></a>
-      			<a href="https://twitter.com/KenzoJam"><i className="icon fa fa-twitter fa-2x"></i></a>
-      			<a href="https://github.com/neotriz"><i className="icon fa fa-github fa-2x"></i></a>
-      		</div>
         </div>
       </div>
     )
