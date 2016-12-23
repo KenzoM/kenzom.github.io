ae5240f65370ecb6ec1e7abd9a89553b8435db18
Kenzo Mendoza <kenzom954@gmail.com>
2016-12-23 17:02:05 -0500

    removed intro section

--- src/components/about.js
+++ src/components/about.js
@@ -3,15 +3,14 @@ import Scroll from 'react-scroll';
 const Element  = Scroll.Element;
 const Link   = Scroll.Link;
 
-
 export default class About extends Component {
   render(){
     return(
-      <Element name="about-section" className="element">
+      <Element name="intro-section" className="element">
         <div className="container">
-          <div className="row">
+          <div className="row about">
             <div className="col-sm-12 text-center">
-              <h1>About</h1>
+              <h1>My Portfolio</h1>
               <hr className="highlight-text" />
             </div>
             <div className="col-sm-12 col-md-6 text-center">
@@ -20,7 +19,7 @@ export default class About extends Component {
             <div className="col-sm-12 col-md-6 text-center">
               <h3>Hi! My name is <span className="highlight-text">Kenzo</span></h3>
               <p className="about-bio">I&#39;ve learned how to program
-                through various resources, most notably <a href="https://www.freecodecamp.com"> <span className="highlight-text2">FreeCodeCamp</span></a>,
+                through various resources, most notably <a href="https://www.freecodecamp.com"> <span className="highlight-text2">FreeCodeCamp</span></a>,&nbsp;
                 <a href="https://wwww.udemy.com"><span className="highlight-text2">Udemy</span></a> and <a href="https://www.udacity.com"><span className="highlight-text2">Udacity</span></a>.
                 <br />
                 My concentration of studies is in front-end development but I am currently working on back-end development
--- src/components/header.js
+++ src/components/header.js
@@ -27,9 +27,6 @@ export default class Header extends Component{
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav-cursor nav navbar-nav navbar-right">
                 <li>
-                  <Link activeClass="active" className="About" to="about-section" spy={true} smooth={true} offset={-45} duration={1000} >About</Link>
-                </li>
-                <li>
                   <Link activeClass="active" className="Skills" to="skills-section" spy={true} smooth={true} offset={-45} duration={1000} >Skills</Link>
                 </li>
                 <li>
